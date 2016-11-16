162fa889dcdd7c091edef09389d3ece0472ff241
Gubenkov Eugene <gubenkoved@gmail.com>
2016-11-16 17:29:12 +0300

    Adding move priority concept

--- src/GameAI.Core/Move.cs
+++ src/GameAI.Core/Move.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace GameAI.Core
 {
+    /// <summary>
+    /// Represents abstract game move. Move transfers one game state to another.
+    /// </summary>
     public abstract class Move
     {
+        /// <summary>
+        /// Gets or sets the priority of move. Higher priority gives a hint to the engine
+        /// to consider this move first as more perspective.
+        /// </summary>
+        public int Priority = 0;
     }
 }
